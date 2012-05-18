e37ef010c988067edb85db47243382a6d64a8ce3
root <root@xlgao-Inspiron-M5110.(none)>
2012-05-18 21:50:01 +0800

    a.c

---
+++ xlgao/a.c
@@ -0,0 +1,21 @@
+#include<apue.h>
+#include<stdio.h>
+#include<fcntl.h>
+#define BUFFSIZE 4096
+
+int
+main(int argc,char* argv[])
+{
+	int fd1,fd2,n;
+	char buf[BUFFSIZE];
+	if((fd1=open(argv[1],O_RDWR,0777))==-1)
+		err_sys("open error!");
+	fd2=open(argv[2],O_RDWR|O_CREAT,0777);
+	n=read(fd1,buf,BUFFSIZE);
+	write(fd2,buf,n);
+	printf("%d\n",n);
+	if(n<0)
+		err_sys("read error!");
+	
+	exit(0);
+}
